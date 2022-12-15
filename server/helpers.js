ff5a81610566c842d206a90c4fa7532d2336fd94
Boris Karakolev <boriskarakolev@hotmail.com>
2022-12-15 20:07:37 +0100

    Fix dates

--- server/helpers.js
+++ server/helpers.js
@@ -1,12 +1,34 @@
 const axios = require("axios");
 
+const getDate = (date) => {
+  date = new Date(date);
+  return `${date.getFullYear().toString()}-${date.getMonth().toString()}-${date
+    .getDate()
+    .toString()}`;
+};
+
+const getTime = (time) => {
+  time = new Date(time);
+  return `${time.getHours().toString()}:${time.getMinutes().toString()}${time
+    .getSeconds()
+    .toString()}`;
+};
+
 const getSlots = async (url) => {
   try {
-    const slots = await axios.get(`${url}bookinggrid?fromDate=2022-12-01T23%3A00%3A00.000Z&days=30`);
+    const slots = await axios.get(
+      `${url}bookinggrid?fromDate=2022-11-30T23%3A00%3A00.000Z&days=31`
+    );
 
     const sortedSlots = slots.data.days.map((day) => {
-      day.slotDate = day.slotDate.split("T")[0];
-      day["slotPools"] = slots.data.slotPools[0].slots;
+      day.slotDate = getDate(day.slotDate);
+      day["slotPools"] = slots.data.slotPools[0].slots.map((slot) => {
+        return {
+          from: `${parseInt(slot.from)}:00`,
+          to: `${parseInt(slot.to)}:00`,
+        };
+      });
+
       return day;
     });
 
@@ -18,21 +40,21 @@ const getSlots = async (url) => {
 
 const getBookedSlots = async (url) => {
   try {
-    const bookedSlots = await axios.get(`${url}resourcebookings?from=2022-12-01T23%3A00%3A00.000Z&days=30`);
+    const bookedSlots = await axios.get(
+      `${url}resourcebookings?from=2022-11-30T23%3A00%3A00.000Z&days=31`
+    );
 
-    const sortedBookedSlots = bookedSlots.data
-      .map((slot) => {
-        slot.startTs = {
-          date: slot.startTs.split("T")[0],
-          time: slot.startTs.split("T")[1].slice(0, 8),
-        };
-        slot.endTs = {
-          date: slot.endTs.split("T")[0],
-          time: slot.endTs.split("T")[1].slice(0, 8),
-        };
-        return slot;
-      })
-      .sort((a, b) => a.startTs.date.localeCompare(b.startTs.date));
+    const sortedBookedSlots = bookedSlots.data.map((slot) => {
+      slot.startTs = {
+        date: getDate(slot.startTs),
+        time: getTime(slot.startTs),
+      };
+      slot.endTs = {
+        date: getDate(slot.endTs),
+        time: getTime(slot.endTs),
+      };
+      return slot;
+    });
 
     return sortedBookedSlots;
   } catch (error) {
